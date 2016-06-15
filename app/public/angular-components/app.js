30d4669becfe2734176333e369d8178bdd7f812b
Hellon Canella <helloncanella@gmail.com>
2016-06-15 17:03:53 -0300

    end of day

--- app/public/angular-components/app.js
+++ app/public/angular-components/app.js
@@ -1,12 +1,47 @@
 angular.module('app',[])
 
-.controller('appController', ['$scope',function($scope){
-  $scope.suggestions = [];
+.controller('appController', ['$scope','$http',function($scope,$http){
   $scope.locationInput =  '';
+  $scope.agendaIsLoading = false;
+
+  $scope.selectAddress = function(address){
+    $scope.suggestions = [];
+    $scope.chosenAddress = address;
+    $scope.calculateAvailability();
+  };
+
+  $scope.calculateAvailability = function(){
+    return new Promise(function (resolve, reject) {
+      $scope.agendaIsLoading = true;
+      $scope.$apply();
+      resolve();
+    });
+  };
+
   $scope.lookForAddress = function(){
-    if(!$scope.locationInput){
-      scope.suggestions = [];
-    }else
+    return new Promise(function(resolve,reject){
+      $http({
+        method: 'get',
+        url: '/address',
+        params: {
+          input:$scope.locationInput
+        }
+      }).then(function(response){
+        var predictions = response.data.predictions;
+        $scope.suggestions = [];
+
+        predictions.forEach(function(content, index){
+          $scope.suggestions[index] = content.description;
+        });
+
+        resolve();
+
+      });
+    });
+
+
+
+
   };
 }])
 
--- test/angular-agenda.js
+++ test/angular-agenda.js
@@ -1,95 +1,130 @@
-describe('Scheduling class', function () {
+describe('Scheduling class', function() {
 
-  var scope, ctrl;
+  var scope,
+    http,
+    ctrl,
+    window;
 
   beforeEach(module('app'));
 
-  beforeEach(inject(function($controller, $rootScope){
+  beforeEach(inject(function($controller, $rootScope, $window) {
     scope = $rootScope.$new();
-    ctrl = $controller('appController',{$scope:scope});
+    ctrl = $controller('appController', {$scope: scope});
+    window = $window;
   }));
 
-  describe('address finder', function () {
-    describe('suggestions', function () {
-      it('not show if input is empty', function () {
-        scope.lookForAddress().then(function(){
+  describe('address finder', function() {
+    var request;
+
+    beforeEach(inject(function($httpBackend) {
+      http = $httpBackend;
+    }));
+
+    describe('suggestions', function() {
+      it('not show if input is empty', function(done) {
+        scope.locationInput = '';
+        setRequest(scope.locationInput);
+
+        scope.lookForAddress().then(function() {
           expect(scope.suggestions.length).toBe(0);
+          done();
         });
+
+        http.flush();
       });
 
-      xit('show when the input isn\'t empty',function(){
-        expect(scope.suggestions.length).toEqual(0);
-        scope.locationInput = 'Rua da';
-        var camilo = scope.lookForAddress();
-        expect(scope.suggestions.length).toBeGreaterThan(0);
-      });
-    });
+      it('show when the input is not empyt', function(done) {
+        scope.locationInput = 'Grajaú';
+        setRequest(scope.locationInput);
 
-
-
-
-    describe('suggestions when clicked', function () {
-      it('set the address as chosen', function () {
-
-      });
-      it('it disapears', function () {
-
-      });
-      it('it triggers the agenda loading', function () {
-
-      });
-    });
-  });
-
-  describe('load spinner', function () {
-    describe('agenda loader', function () {
-      it('works when the the calendar events isn\'t downloaded', function () {
-      });
-    });
-    describe('availability loader', function () {
-      it('works when the the availability isn\'t calculated', function () {
-      });
-    });
-  });
-
-  describe('agenda', function () {
-    it('just appears when the address is chose', function () {
-
-    });
-
-    describe('dayLines', function () {
-      it('shows 15 from now', function () {
-
-      });
-
-      describe('day', function () {
-        it('when clicked, show row of respective availability', function () {
-
+        scope.lookForAddress().then(function() {
+          expect(scope.suggestions.length).toBe(2);
+          done();
         });
-      });
-    });
-    describe('availability row', function () {
-      it('is shown when the respective day is chosen', function () {
+
+        http.flush();
 
       });
-    });
-  });
-  describe('schedule button', function () {
-    it('if a day is chosen, when clicked trigger the process of scheduling', function () {
+
+      function setRequest(locationInput) {
+        var response;
+
+        locationInput = window.encodeURI(locationInput).replace(/%20/g, "+");
+
+        if(locationInput){
+          response = {predictions:[{'description':''},{'description':''}]};
+        }else{
+          response = {predictions:[]};
+        }
+
+        response = JSON.stringify(response);
+        http.when('GET', '/address?input=' + locationInput).respond(200, response);
+      }
 
     });
-  });
 
-  describe('success page', function () {
-    it('when the class is scheduled with success', function () {
+    describe('suggestions when clicked', function() {
+      var address;
 
+      beforeEach(function(){
+        spyOn(scope,'calculateAvailability').and.callThrough();
+        address = 'Rua das Marrecas';
+        scope.suggestions = ['','']; //simulate the existence of sugestions
+        scope.agendaIsLoading = false;
+        scope.selectAddress(address);
+      });
+
+
+      it('set chosen address', function(done) {
+        expect(scope.chosenAddress).toBe(address);
+        done();
+      });
+
+      it('it disapears', function(done) {
+        expect(scope.suggestions.length).toBe(0);
+        done();
+      });
+
+      it('it triggers the agenda loading', function(done) {
+        expect(scope.calculateAvailability).toHaveBeenCalled();
+        expect(scope.agendaIsLoading).toBeTruthy();
+        done();
+      });
     });
   });
 
-  describe('error page', function () {
-    it('when no class is arranged', function () {
-
+  describe('load spinner', function() {
+    describe('agenda loader', function() {
+      it('works when the the calendar events isn\'t downloaded', function() {});
+    });
+    describe('availability loader', function() {
+      it('works when the the availability isn\'t calculated', function() {});
     });
   });
 
+  describe('agenda', function() {
+    it('just appears when the address is chose', function() {});
+
+    describe('dayLines', function() {
+      it('shows 15 from now', function() {});
+
+      describe('day', function() {
+        it('when clicked, show row of respective availability', function() {});
+      });
+    });
+    describe('availability row', function() {
+      it('is shown when the respective day is chosen', function() {});
+    });
+  });
+  describe('schedule button', function() {
+    it('if a day is chosen, when clicked trigger the process of scheduling', function() {});
+  });
+
+  describe('success page', function() {
+    it('when the class is scheduled with success', function() {});
+  });
+
+  describe('error page', function() {
+    it('when no class is arranged', function() {});
+  });
 });
